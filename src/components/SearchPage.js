83cf463fd8188affb81be2aba30b5c0552bc0931
Mubeen Kodvavi <mubeen.kodvavi@gmail.com>
2021-08-22 11:03:01 +0500

    search bar

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+import "../BooksAPI";
+import { search } from "../BooksAPI";
 
 class SearchPage extends Component {
+  state = { books: [] };
   exitSearch = (event) => {
     this.props.returnMain();
   };
+
+  onSearchSubmit = async (term) => {
+    const response = await search(term);
+    console.log(response);
+  };
+
   render() {
     return (
       <div className="search-books">
@@ -11,20 +21,7 @@ class SearchPage extends Component {
           <button className="close-search" onClick={this.exitSearch}>
             Close
           </button>
-          <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <input type="text" placeholder="Search by title or author" />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid" />
+          <SearchBar onSubmit={this.onSearchSubmit} />
         </div>
       </div>
     );
